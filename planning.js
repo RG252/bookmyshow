9867f902b857d230ff1b3e776f08530fc7a9ad44
Rohan Gupta <guptarohan2012@gmail.com>
2021-07-13 15:32:09 +0530

    first commit

---
+++ planning.js
@@ -0,0 +1,19 @@
+//book my show planning
+
+// -> we need a reusable carousel
+//  -> hero carousel
+//  -> common carousel
+//  -> cast and crew small carousel
+
+//pages
+//  -> home page
+//  -> movie page
+//  -> category page
+//  -> auth page
+//  -> razorpay payment gateway
+
+//plan-> How are we building it??🧐
+//layouts
+//routes
+//pages
+//components
